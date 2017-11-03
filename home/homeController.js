4abb4b17feeabb03cb90ea401f314e6bee7848b5
Thomas Lowry <minilao94@yahoo.com>
2017-11-03 14:07:02 -0600

    its working

--- home/homeController.js
+++ home/homeController.js
@@ -1,18 +1,89 @@
 angular.module('trivia').controller('homeController', function ($scope, homeService) {
     $scope.test = 'test scope'
     $scope.myfilter = ''
+    $scope.search = true;
     console.log('controller')
     homeService.getQuestions().then(function (questions) {
         console.log(questions.data)
-        $scope.questions = questions.data
+        $scope.questions = questions.data.map(q => {
+            switch (q.difficulty) {
+                case 1:
+                    q.difficulty = 'Easy'
+                    break;
+                case 2:
+                    q.difficulty = 'Medium'
+                    break;
+                case 3:
+                    q.difficulty = 'Hard'
+                default:
+                    break;
+            }
+            return q
+        })
     })
-    // $scope.setFilter = function (n) {
-    //     console.log(n)
-    //     $scope.myfilter = n
-    //     $scope.questions = homeService.getQuestions().then(response => {
-    //         $scope.questions = response.data.filter(q => q.difficulty === n)
-    //         console.log($scope.questions)
-            
-    //     })
-    // }
+    $scope.selectOption = function (question, option) {
+        console.log(option)
+        console.log(question)
+        question.chosen = option
+        if (option == question.options[question.correct_answer]) {
+            console.log('true')
+            question.correct = true
+            question.class = "Easy"
+        }
+        else {
+            console.log('false')
+            question.correct = false
+            question.class = "Hard"
+        }
+    }
+    $scope.toggleSearch = function () {
+        console.log($scope.search)
+        $scope.search = !$scope.search
+    }
+    $scope.setFilter = function (n) {
+        console.log(n)
+        $scope.myfilter = n
+        if (n) {
+            homeService.getQuestions().then(response => {
+                $scope.questions = response.data.filter(q => q.difficulty === n)
+                console.log($scope.questions)
+                $scope.questions = $scope.questions.map(q => {
+                    switch (q.difficulty) {
+                        case 1:
+                            q.difficulty = 'Easy'
+                            break;
+                        case 2:
+                            q.difficulty = 'Medium'
+                            break;
+                        case 3:
+                            q.difficulty = 'Hard'
+                        default:
+                            break;
+                    }
+                    return q
+                })
+            }
+            )
+        }
+        else {
+            homeService.getQuestions().then(response => {
+                $scope.questions = response.data
+                $scope.questions = $scope.questions.map(q => {
+                    switch (q.difficulty) {
+                        case 1:
+                            q.difficulty = 'Easy'
+                            break;
+                        case 2:
+                            q.difficulty = 'Medium'
+                            break;
+                        case 3:
+                            q.difficulty = 'Hard'
+                        default:
+                            break;
+                    }
+                    return q
+                })
+            })
+        }
+    }
 })
